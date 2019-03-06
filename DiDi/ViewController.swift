c51f1b9cc449e6e9fbafe5c9e36e54bffd3bb5e2
xumingyou <xumingyou@xumingyoudeMacBook-Pro.local>
2019-03-06 14:59:07 +0800

    chag

--- DiDi/ViewController.swift
+++ DiDi/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func viewWillAppear(_ animated: Bool) {
-        print()
+        print("1234")
     }
 }
 
