f56c751932733a7b7c2607be6645e10560a155dd
xumingyou <xumingyou@xumingyoudeMacBook-Pro.local>
2019-03-06 14:51:12 +0800

    添加方法

--- DiDi/ViewController.swift
+++ DiDi/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-你是个大混蛋
-
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
 }
 
